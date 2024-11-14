f530a5cfa88f651027a28a7d8f2c88f8bacea428
Jahir <93404854+JahirWH@users.noreply.github.com>
2024-11-14 12:29:36 -0600

    update
    add img

---
+++ js.js
@@ -0,0 +1,9 @@
+function open(){
+	var ver = document.getElementsByTagName("uno_1");
+	var oculta = document.getElementsByTagName("uno");
+
+	if (ver.style.display= "block") {
+		ver.style.display = "none";
+		oculta.style.display = "block";
+
+	}
