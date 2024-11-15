8675c6927f71c0a7ccbc68aaed5b0f8d4cb38980
Jahir <93404854+JahirWH@users.noreply.github.com>
2024-11-14 19:29:49 -0600

    correccion

--- js.js
+++ js.js
@@ -1,9 +1,12 @@
-	
-	function open(){
-	    var a = document.getElementById("img1");
-		var b = document.getElementById("img2");
-		if (8 > 3){
-			b.style.visibility = "visible";
-		}
-}
+function modal_1(){
+	var d1 = document.getElementById("img1");
+	var d2 = document.getElementById("img2");
 
+	if (d1.style.display == "block" ) {
+		d1.style.display = "none"
+		d2.style.display = "block"
+	}else{
+		d1.style.display = "none"
+
+	}
+}
