df14c9375e82b09abd37ccd08de5ffc861c29410
Jahir <93404854+JahirWH@users.noreply.github.com>
2024-11-15 22:57:38 -0600

    update
    add img

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 //agrega clase
 
 
-function modal(){
+function modal1(){
 	var d = document.getElementById("img1");
 	var a = document.getElementById("img2");	
 
@@ -14,9 +14,37 @@ function modal(){
 }
 //anadir clase de clase//
 
-function modal1(){
+function modal2(){
 	var d = document.getElementById("img1");
 	var a = document.getElementById("img2");	
+
+    if (9>2){
+		a.classList.remove("img1_show");
+		a.classList.toggle("img1_oculta");
+		d.classList.remove("img1_oculta");
+		d.classList.toggle("img1_show");
+	}
+
+}
+
+
+
+function modal3(){
+	var d = document.getElementById("img3");
+	var a = document.getElementById("img4");	
+
+    if (9>2){
+		d.classList.remove("img1_show");
+		d.classList.toggle("img1_oculta");
+		a.classList.remove("img1_oculta");
+		a.classList.toggle("img1_show");
+	}
+}
+
+
+function modal4(){
+	var d = document.getElementById("img3");
+	var a = document.getElementById("img4");	
 
     if (9>2){
 		a.classList.remove("img1_show");
