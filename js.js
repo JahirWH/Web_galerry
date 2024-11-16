659e7f201324281382046ebd4913d995fa7e8840
Jahir <93404854+JahirWH@users.noreply.github.com>
2024-11-16 08:08:40 -0600

    update
    reduccion de codigo y ordenamiento

--- js.js
+++ js.js
@@ -1,56 +1,50 @@
-//agrega clase
+//cambio de none a block en div
+// reduccion de codigo
+function zom(){
+	var z = document.getElementById("zom");
+	if (z.style.display == "none") {
+		z.style.display = "block";
+	}else{
+		z.style.display = "none";
+	}
+
+}
+
+
+
+
+//cambio de imagenes 
 
 
 function modal1(){
+
 	var d = document.getElementById("img1");
 	var a = document.getElementById("img2");	
 
-    if (9>2){
-		d.classList.remove("img1_show");
-		d.classList.toggle("img1_oculta");
-		a.classList.remove("img1_oculta");
-		a.classList.toggle("img1_show");
+    if (d.style.display == "block"){
+		d.style.display = "none"
+		a.style.display = "block";
+	}else {
+		a.style.display = "none"
+		d.style.display = "block";
 	}
 }
+
+
+
 //anadir clase de clase//
 
-function modal2(){
-	var d = document.getElementById("img1");
-	var a = document.getElementById("img2");	
+//function modal2(){
+//	var d = document.getElementById("img1");
+//	var a = document.getElementById("img2");	
 
-    if (9>2){
-		a.classList.remove("img1_show");
-		a.classList.toggle("img1_oculta");
-		d.classList.remove("img1_oculta");
-		d.classList.toggle("img1_show");
-	}
+  //  if (9>2){
+	//	a.classList.remove("img1_show");
+	//	a.classList.toggle("img1_oculta");
+	//	d.classList.remove("img1_oculta");
+	//	d.classList.toggle("img1_show");
+//	}
 
-}
-
-
-
-function modal3(){
-	var d = document.getElementById("img3");
-	var a = document.getElementById("img4");	
-
-    if (9>2){
-		d.classList.remove("img1_show");
-		d.classList.toggle("img1_oculta");
-		a.classList.remove("img1_oculta");
-		a.classList.toggle("img1_show");
-	}
-}
+//}
 
 
-function modal4(){
-	var d = document.getElementById("img3");
-	var a = document.getElementById("img4");	
-
-    if (9>2){
-		a.classList.remove("img1_show");
-		a.classList.toggle("img1_oculta");
-		d.classList.remove("img1_oculta");
-		d.classList.toggle("img1_show");
-	}
-
-}
