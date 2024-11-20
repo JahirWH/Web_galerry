f3ad82f6d78c01a2bf0e7c8341a5f4d7fc61d2d8
Jahir <93404854+JahirWH@users.noreply.github.com>
2024-11-20 00:45:35 -0600

    update

--- js.js
+++ js.js
@@ -3,79 +3,9 @@
 
 	//cambio de imagenes en el mismo div
 function imagen1(){
-	a = document.getElementsByClassName("i1");
-	d = document.getElementsByClassName("i2");
-	c = document.getElementsByClassName("i3");
-	e = document.getElementsByClassName("i4");
-	f = document.getElementsByClassName("i5");
-	g = document.getElementsByClassName("i6");
-	h = document.getElementsByClassName("i7");
-	i = document.getElementsByClassName("i8");
-	j = document.getElementsByClassName("i9");
-
-
-
-	if (a.style.display == "none",
-		d.style.display == "none",
-		c.style.display == "none",
-		e.style.display == "none",
-		f.style.display == "none",
-		g.style.display == "none",
-		h.style.display == "none",
-		i.style.display == "none",
-		j.style.display == "none") {
-		a.style.display = "block";
-}else {
-		a.style.display = "none",		
-		d.style.display = "none",
-		c.style.display = "none",
-		e.style.display = "none",
-		f.style.display = "none",
-		g.style.display = "none",
-		h.style.display = "none",
-		i.style.display = "none",
-		j.style.display = "none",
-		imagen1()
-		
+	
 }
-}
-function imagen2(){
 
-	a = document.getElementsByClassName("i1");
-	d = document.getElementsByClassName("i2");
-	c = document.getElementsByClassName("i3");
-	e = document.getElementsByClassName("i4");
-	f = document.getElementsByClassName("i5");
-	g = document.getElementsByClassName("i6");
-	h = document.getElementsByClassName("i7");
-	i = document.getElementsByClassName("i8");
-	j = document.getElementsByClassName("i9");
-
-
-	if (a.style.display == "none",
-		d.style.display == "none",
-		c.style.display == "none",
-		e.style.display == "none",
-		f.style.display == "none",
-		g.style.display == "none",
-		h.style.display == "none",
-		i.style.display == "none",
-		j.style.display == "none"
-		) {
-		d.style.display = "block";
-}else {
-		a.style.display = "none",
-		d.style.display = "none",
-		c.style.display = "none",
-		e.style.display = "none",
-		f.style.display = "none",
-		g.style.display = "none",
-		h.style.display = "none",
-		i.style.display = "none",
-		j.style.display = "none"
-		imagen2()
-}
-}
 
 
 
@@ -93,6 +23,8 @@ function zom(){
 
 
 
+
+
 	//cambio de imagenes antigua a nueva 
 
 
@@ -100,6 +32,7 @@ function modal1(){
 
 	var d = document.getElementById("img1");
 	var a = document.getElementById("img2");	
+
 
 	if (d.style.display == "block"){
 		d.style.display = "none"
@@ -125,6 +58,20 @@ function modal2(){
 	}
 }
 
+function modal3(){
+
+	var d = document.getElementById("img5");
+	var a = document.getElementById("img6");	
+
+
+	if (d.style.display == "block"){
+		d.style.display = "none"
+		a.style.display = "block";
+	}else {
+		a.style.display = "none"
+		d.style.display = "block";
+	}
+}
 
 	// subida de imagen
 function up() {
